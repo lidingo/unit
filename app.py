7b479c1899b42302b41ad0280f8529776ca08fcb
lidingo <144531836+lidingo@users.noreply.github.com>
2023-09-10 14:10:49 +0200

    App

---
+++ app.py
@@ -0,0 +1,9 @@
+async def application(scope, receive, send):
+    await send({
+        'type': 'http.response.start',
+        'status': 200
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello, ASGI\n'
+    })
