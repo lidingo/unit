5726f69b78cded86990ce4662fbbbbf8a46328f9
lidingo <144531836+lidingo@users.noreply.github.com>
2023-09-10 16:48:12 +0200

    Support lifespan protocol.

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 async def application(scope, receive, send):
+    # See https://asgi.readthedocs.io/en/latest/specs/lifespan.html
+    if scope['type'] == 'lifespan':
+        while True:
+            message = await receive()
+            if message['type'] == 'lifespan.startup':
+                ...  # Do some startup here!
+                await send({'type': 'lifespan.startup.complete'})
+            elif message['type'] == 'lifespan.shutdown':
+                ...  # Do some shutdown here!
+                await send({'type': 'lifespan.shutdown.complete'})
+                return
+
     await send({
         'type': 'http.response.start',
         'status': 200
